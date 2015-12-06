85edb25c48068965af95f9153496047b6046c11b
mariodiar <mariodiar@gmail.com>
2015-12-05 22:50:29 -0600

    fetches comments from reddit api

--- backend/ver0/backend.go
+++ backend/ver0/backend.go
@@ -9,20 +9,15 @@ import (
 "appengine/urlfetch"
 )
 
-//struct of reddit json api
-//Respons is a slice because for some reason, the reddit api starts as an array
+//Response is a slice because the reddit api starts as an array with two objects
 type Response []struct {
-	Parent struct {
-		Data struct {
-			Children []struct {
-				Com Comment
-			}
-		}
-	}
-}
-//struct of comment
-type Comment struct {
-	Name string `json:"body"`
+	Data struct {
+		Children []struct {
+			Comments struct {
+					Body string `json:"body"`
+				}`json:"data"`
+		} `json:"children"`
+	} `json:"data"`
 }
 
 func init() {
@@ -30,20 +25,29 @@ func init() {
 }
 
 func getComments(w http.ResponseWriter, r *http.Request) {
-
+	var comments []string
 	url := "https://www.reddit.com/r/videos/comments/3vgdsb/recruitment_2016.json"
+
 	//starting newContext and getting data from the url
 	c := appengine.NewContext(r)
 	client := urlfetch.Client(c)
 	resp, err := client.Get(url)
 	if err != nil { fmt.Fprint(w, "Error client.Get(): ", err) }
+	
 	//creating new response object
 	re := new(Response)
 	fmt.Fprint(w, "Response struct: ", re, "\n")
+
 	//decoding the data to json into our Response array
 	errTwo := json.NewDecoder(resp.Body).Decode(&re)
 	if errTwo != nil {  fmt.Fprint(w, "Error decoding: ", errTwo, "\n") }
 
-	fmt.Fprint(w, "Response struct: ", re)
+	for _, el := range (*re)[1].Data.Children {
+		comments = append(comments, el.Comments.Body)
+	}
+
+	for _, el := range comments {
+		fmt.Fprint(w, el, "\n|||||||||||||||||||||||||||||||||||||||||||\n")
+	}
 }
 
