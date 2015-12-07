30fb9e356f5e07e40746e2805151bd8f486f4fed
mariodiar <mariodiar@gmail.com>
2015-12-07 01:10:28 -0600

    Gets hidden comments on reddit threads

--- backend/ver0/backend.go
+++ backend/ver0/backend.go
@@ -1,12 +1,11 @@
 package backend
 
 import (
-"fmt"
-"net/http"
-"encoding/json"
-
-"appengine"
-"appengine/urlfetch"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"appengine"
+	"appengine/urlfetch"
 )
 
 //Response is a slice because the reddit api starts as an array with two objects
@@ -14,40 +13,72 @@ type Response []struct {
 	Data struct {
 		Children []struct {
 			Comments struct {
-					Body string `json:"body"`
-				}`json:"data"`
+				Body string `json:"body"`
+			} `json:"data"`
 		} `json:"children"`
 	} `json:"data"`
 }
 
 func init() {
 	http.HandleFunc("/api/getcomments", getComments)
+	// http.HandleFunc("/api/test", testHiddenComment)
 }
 
-func getComments(w http.ResponseWriter, r *http.Request) {
+func fetchComments(c appengine.Context, url string) ([]string, error) {
 	var comments []string
-	url := "https://www.reddit.com/r/videos/comments/3vgdsb/recruitment_2016.json"
 
-	//starting newContext and getting data from the url
-	c := appengine.NewContext(r)
 	client := urlfetch.Client(c)
-	resp, err := client.Get(url)
-	if err != nil { fmt.Fprint(w, "Error client.Get(): ", err) }
-	
-	//creating new response object
-	re := new(Response)
-	fmt.Fprint(w, "Response struct: ", re, "\n")
+	res, err := client.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Error Get: %v", err)
+	}
 
-	//decoding the data to json into our Response array
-	errTwo := json.NewDecoder(resp.Body).Decode(&re)
-	if errTwo != nil {  fmt.Fprint(w, "Error decoding: ", errTwo, "\n") }
+	content := new(Response)
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&content)
+	if err != nil {
+		return nil, fmt.Errorf("Error decoding: %v", err)
+	}
 
-	for _, el := range (*re)[1].Data.Children {
+	for _, el := range (*content)[1].Data.Children {
 		comments = append(comments, el.Comments.Body)
 	}
 
+	return comments, nil
+
+}
+
+func fetchHiddenComment(c appengine.Context, url string) (string, error) {
+	comment := ""
+	client := urlfetch.Client(c)
+	res, err := client.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("Error Get: %v", err)
+	}
+
+	content := new(Response)
+	dec := json.NewDecoder(res.Body)
+	err = dec.Decode(&content)
+	if err != nil {
+		return "", fmt.Errorf("Error decodign: %v", err)
+	}
+
+	comment = (*content)[1].Data.Children[0].Comments.Body
+	return comment, nil
+}
+
+func getComments(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	url := "https://www.reddit.com/r/AskReddit/comments/2pq2iz/reddit_what_song_consistently_gives_you_the_chills.json?limit=500"
+
+	comments, err := fetchComments(c, url)
+	if err != nil {
+		http.Error(w, "Reddit API not working", http.StatusInternalServerError)
+		c.Errorf("Fetching comments: %v", err)
+	}
+
 	for _, el := range comments {
-		fmt.Fprint(w, el, "\n|||||||||||||||||||||||||||||||||||||||||||\n")
+		fmt.Fprint(w, el, "\n\n")
 	}
 }
 
